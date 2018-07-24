6bd07c14e7fb3dc33f0b13b91c2bb0e8fead40dd
CNKI <wohudianwei@163.com>
2018-07-24 11:43:33 +0800

    汉字错了

--- src/main/java/com.JavaWebTest/controller/UserController.java
+++ src/main/java/com.JavaWebTest/controller/UserController.java
@@ -3,13 +3,20 @@ package com.JavaWebTest.controller;
 import com.JavaWebTest.model.User;
 import com.JavaWebTest.service.IUserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/user")
@@ -19,6 +26,7 @@ public class UserController {
 
     @RequestMapping("/showUser.do")
     public void selectUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         long userId = Long.parseLong(request.getParameter("id"));
@@ -26,5 +34,23 @@ public class UserController {
         ObjectMapper mapper = new ObjectMapper();
         response.getWriter().write(mapper.writeValueAsString(user));
         response.getWriter().close();
+    }
+    @InitBinder
+    public void initBinder(ServletRequestDataBinder binder){
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"),
+                true));
+    }
+    @ExceptionHandler
+    public ModelAndView exceptionHandler(Exception ex){
+        ModelAndView mv = new ModelAndView("error");
+        mv.addObject("exception", ex);
+        System.out.println("in testExceptionHandler");
+        return mv;
+    }
+
+    @RequestMapping("/error")
+    public String error(){
+        int i = 5/1;
+        return "hello";
     }
 }
